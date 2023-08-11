f8c40ab1ad74ed7b97251396968c46ecf6a1658d
danielpqb <danielpqb@gmail.com>
2023-08-11 00:48:44 -0300

    Selectable header section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,19 @@
-import { ComponentProps } from "react";
+"use client";
+
+import { sections } from "@/data/sections";
+import { ComponentProps, Dispatch, SetStateAction, useState } from "react";
 
 interface HeaderProps extends ComponentProps<"header"> {}
-
 export function Header(props: HeaderProps) {
+  const [selectedSection, setSelectedSection] = useState("concept");
+
   return (
     <header className="w-full h-32 flex flex-col fixed z-10 bg-white-primary">
       <img
         src="/images/main-logo.svg"
         className="absolute top-4 left-0 right-0 m-auto"
       ></img>
-      <div className="w-full h-24 relative flex justify-center font-roboto">
+      <div className="w-full h-24 relative flex justify-center font-roboto font-semibold text-[0.875rem]">
         <div className="w-full max-w-6xl flex justify-end">
           <div className="flex items-center">
             <button className="bg-blue-secondary text-white-primary px-4 py-2">
@@ -17,22 +21,48 @@ export function Header(props: HeaderProps) {
             </button>
           </div>
           <div className="flex items-center">
-            <button className="px-4 py-2 font-semibold">Login</button>
+            <button className="px-4 py-2">Login</button>
           </div>
           <div className="flex items-center">
-            <button className="px-4 py-2 font-semibold">O</button>
+            <button className="px-4 py-2">O</button>
           </div>
         </div>
       </div>
       <div className="flex flex-1 items-center justify-center">
-        <span className="px-6">Conceito</span>
-        <span className="px-6">Valetecast</span>
-        <span className="px-6">Eventos</span>
-        <span className="px-6">Colunistas</span>
-        <span className="px-6">Fundadores</span>
-        <span className="px-6">Dúvidas</span>
+        {sections.map((section, index) => {
+          return (
+            <SectionLabel
+              section={section}
+              key={index}
+              className="px-6 border-b-[4px] border-black-primary"
+              selectedSection={selectedSection}
+              setSelectedSection={setSelectedSection}
+            />
+          );
+        })}
       </div>
       <div className="border-b-[0.0625rem] border-b-brown-primary"></div>
     </header>
+  );
+}
+
+interface SectionLabelProps extends ComponentProps<"button"> {
+  section: { id: string; title: string };
+  selectedSection: string;
+  setSelectedSection: Dispatch<SetStateAction<string>>;
+}
+function SectionLabel(props: SectionLabelProps) {
+  return (
+    <button
+      onClick={() => {
+        props.setSelectedSection(props.section.id);
+      }}
+      className={`px-6 h-full ${
+        props.selectedSection === props.section.id &&
+        "border-b-[0.125rem] border-black-primary "
+      }`}
+    >
+      {props.section.title}
+    </button>
   );
 }
---
+++ src/data/sections.ts
@@ -0,0 +1,8 @@
+export const sections = [
+  { id: "concept", title: "Conceito" },
+  { id: "valetecast", title: "Valetecast" },
+  { id: "events", title: "Eventos" },
+  { id: "columnists", title: "Colunistas" },
+  { id: "founders", title: "Fundadores" },
+  { id: "doubts", title: "Dúvidas" },
+];
